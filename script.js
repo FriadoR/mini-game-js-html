804853d6526e8b510f2f37da302690f3768f168e
s.sachenko <eu.friador@yandex.ru>
2022-10-02 23:14:05 +0300

    fixed

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 const cards = document.querySelectorAll(".card");
 
-let matchedCard = 0;
+let matched = 0;
 let cardOne, cardTwo;
 let disableDeck = false;
 
 // click for all cards
 
-function flipCard(e) {
-  let clickedCard = e.target; // get user click on card
-
-  if (clickedCard !== cardOne && !disableDeck) {
+function flipCard({ target: clickedCard }) {
+  if (cardOne !== clickedCard && !disableDeck) {
     clickedCard.classList.add("flip");
-
     if (!cardOne) {
       // return cardOne value to clickedCard
       return (cardOne = clickedCard);
     }
     cardTwo = clickedCard;
     disableDeck = true;
-    let cardOneImg = cardOne.querySelector("img").src,
-      cardTwoImg = cardTwo.querySelector("img").src;
+    let cardOneImg = cardOne.querySelector(".back-view img").src,
+      cardTwoImg = cardTwo.querySelector(".back-view img").src;
     matchCards(cardOneImg, cardTwoImg);
   }
 }
@@ -27,9 +24,9 @@ function flipCard(e) {
 function matchCards(img1, img2) {
   if (img1 === img2) {
     // two cards matched
-    matchCards++; // increment matched value by 1
+    matched++; // increment matched value by 1
 
-    if (matchedCard == 8) {
+    if (matched == 8) {
       // close game, matched 8 * 2 = 16 cards
       setTimeout(() => {
         return shuffleCard;
@@ -57,18 +54,18 @@ function matchCards(img1, img2) {
 }
 
 function shuffleCard() {
-  matchedCard = 0;
-  cardOne = cardTwo = "";
+  matched = 0;
   disableDeck = false;
+  cardOne = cardTwo = "";
 
   // creating array of 16 items and each item is repeated twice
   let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
   arr.sort(() => (Math.random() > 0.5 ? 1 : -1)); // sorting array item randomly
 
-  cards.forEach((card, index) => {
+  cards.forEach((card, i) => {
     card.classList.remove("flip");
     let imgTag = card.querySelector("img");
-    imgTag.src = `/Game Images/img-${arr[index]}.png`;
+    imgTag.src = `/Game Images/img-${arr[i]}.png`;
     card.addEventListener("click", flipCard);
   });
 }
