3450b112ed643f5c57054dfc1cfdc72899572aac
s.sachenko <eu.friador@yandex.ru>
2022-10-02 20:33:32 +0300

    done safe

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const cards = document.querySelectorAll(".card");
 
+let cardOne, cardTwo;
+
 // click for all cards
 
 function flipCard(e) {
-  console.log(e.target);
+  let clickedCard = e.target; // get user click on card
+  clickedCard.classList.add("flip");
 }
 
 cards.forEach((card) => {
