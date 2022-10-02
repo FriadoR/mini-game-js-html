e6eefade5905ac181cfc5b06b9477078c298a435
s.sachenko <eu.friador@yandex.ru>
2022-10-02 21:05:16 +0300

    some special

--- script.js
+++ script.js
@@ -6,7 +6,32 @@ let cardOne, cardTwo;
 
 function flipCard(e) {
   let clickedCard = e.target; // get user click on card
-  clickedCard.classList.add("flip");
+
+  if (clickedCard !== cardOne) {
+    clickedCard.classList.add("flip");
+    if (!cardOne) {
+      // return cardOne value to clickedCard
+      return (cardOne = clickedCard);
+    }
+    cardTwo = clickedCard;
+
+    let cardOneImg = cardOne.querySelector("img").src,
+      cardTwoImg = cardTwo.querySelector("img").src;
+    matchCards(cardOneImg, cardTwoImg);
+  }
+}
+
+function matchCards(img1, img2) {
+  if (img1 === img2) {
+    // two cards matched
+    return console.log("Card matched");
+  }
+
+  // if two card not matched
+  setTimeout(() => {
+    cardOne.classList.add("shake");
+    cardTwo.classList.add("shake");
+  }, 400);
 }
 
 cards.forEach((card) => {
