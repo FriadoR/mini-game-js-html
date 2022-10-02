0ab545cc6f9acf58175c368d34cb2abfbe6d0652
s.sachenko <eu.friador@yandex.ru>
2022-10-02 22:56:14 +0300

    safe

--- script.js
+++ script.js
@@ -27,6 +27,15 @@ function flipCard(e) {
 function matchCards(img1, img2) {
   if (img1 === img2) {
     // two cards matched
+    matchCards++; // increment matched value by 1
+
+    if (matchedCard == 8) {
+      // close game, matched 8 * 2 = 16 cards
+      setTimeout(() => {
+        return shuffleCard;
+      }, 1000); // calling shuffleCard function after 1 sec
+    }
+
     cardOne.removeEventListener("click", flipCard);
     cardTwo.removeEventListener("click", flipCard);
     cardOne = cardTwo = ""; //setting both card value to blank
@@ -46,6 +55,25 @@ function matchCards(img1, img2) {
     disableDeck = false;
   }, 1200);
 }
+
+function shuffleCard() {
+  matchedCard = 0;
+  cardOne = cardTwo = "";
+  disableDeck = false;
+
+  // creating array of 16 items and each item is repeated twice
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+  arr.sort(() => (Math.random() > 0.5 ? 1 : -1)); // sorting array item randomly
+
+  cards.forEach((card, index) => {
+    card.classList.remove("flip");
+    let imgTag = card.querySelector("img");
+    imgTag.src = `/Game Images/img-${arr[index]}.png`;
+    card.addEventListener("click", flipCard);
+  });
+}
+
+shuffleCard();
 
 cards.forEach((card) => {
   card.addEventListener("click", flipCard);
